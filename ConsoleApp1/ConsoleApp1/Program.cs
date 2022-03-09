b8bb4405e232e02d9abb26532bcc8e469184b1e9
Bogumil Latuszek <bogumil.latuszek@microsoft.wsei.edu.pl>
2022-03-09 09:40:39 +0100

    zad10

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("nothing");
+
+            Student[] students =
+            {
+                new Student(){Ects = 10,Name = "Karol"},
+                new Student(){Ects = 10,Name = "Karolina"},
+                new Student(){Ects = 10,Name = "Ania"},
+                new Student(){Ects = 10,Name = "Piotrek"},
+            };
+            Array.Sort(students);
+            foreach(Student s in students)
+            {
+                Console.WriteLine(s.Name);
+            }
+
         }
+
+        //public static explicit operator float(Money money)
     }
+
+    class Student : IComparable<Student>
+    {
+        public int Ects { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            int result = Ects.CompareTo(other.Ects); // int implementuje CompareTo
+            if (result == 0) 
+            { 
+                return Name.CompareTo(other.Name); // string implementuje CompareTo
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+    }
+
 }
